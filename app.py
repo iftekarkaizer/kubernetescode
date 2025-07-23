64bcca806ec8643c38ea89b20771754b07fc5986
Iftekar Bin Kaizer <65841314+iftekarkaizer@users.noreply.github.com>
2025-07-23 04:21:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please dont please, likasdadeasdasdazx, and yoyo commenaastdffgggggggggggon this video, TY!!!'
+    return 'Please dont please, likasdadeasdasdazx, and yoyo commenaastdffgggggggggggon this hjkhkjhkvideo, TY!!!'
